f03bd964ece656ace432f38b0a953a11b73e423a
DeanTang <30429736+DeanTang@users.noreply.github.com>
2025-03-26 20:37:18 +1100

    Added background, player, enemy and hp. Imported TextMesh

--- KindieFighters/Assets/Scripts/PlayerBehaviour.cs
+++ KindieFighters/Assets/Scripts/PlayerBehaviour.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
+
 
 public class PlayerBehaviour : MonoBehaviour
 {
     private int maxhp;
+    private int hp, atk, def;
+    public TMP_Text displayHp;
+
+    public void InitStat(int hpVal, int atkVal, int defVal)
+    {
+        maxhp = hpVal;
+        hp = maxhp;
+        atk = atkVal;
+        def = defVal;
+    }
+
+    public void TakeDmg(int dmg)
+    {
+        hp = hp - dmg;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitStat(50, 10, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        displayHp.text = hp.ToString();
     }
 }
